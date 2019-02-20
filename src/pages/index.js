7a9aa004f65cca25ff2beb81f447ee4e18daeb99
smlc <siesa.marlon@hotmail.fr>
2019-02-20 22:29:19 +0100

    init

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,11 @@
 import React from "react"
+import { auto } from "eol";
 
-export default () => <div>Hello world!</div>
+export default () => (
+    <main style={{ position: `relative`, justifyContent: `center`, alignItems: `center` }}>
+    <div >
+        <label for="summonerName">Summoner Name </label> 
+        <input type="text" id="summonerName" name="summonerName" required/>
+    </div>
+    </main>
+)
